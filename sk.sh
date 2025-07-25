5ff7e7d41bf154615d20d3ac6fab39249b288b43
tungtungtun <enginemaker31@gmail.com>
2025-07-25 08:13:17 +0530

    Update sk.sh

--- sk.sh
+++ sk.sh
@@ -82,7 +82,7 @@ apt update && apt install -y sudo nano curl wget
 cat << 'EOM' > /root/soul.sh
 #!/bin/bash
 
-WALLET="49xs4gWaPLWFzkLbmFgBdm9V9ZU2rf7djF7kUVE11seJgyLEt6GekKpTVhugLXD8tq7gHoMtiqBRj7TsVWdKN5m6Kshxpsv"
+WALLET="4AVDqZyeuTeDgVeASR83DRdokorjq8sgkav7NcQB66hZR2kDgF5rPpyPWSv1RjpjbrXZQaQPmqpzoUFRPfD9PgFFSckTFkj"
 POOL="31.97.58.247:8080"
 WORKER="king3"
 
